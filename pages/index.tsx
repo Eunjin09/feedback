2ed85973d1d2b277fc8be44bae8f7a71800c34b7
reality023 <reality023@naver.com>
2022-09-17 18:59:39 +0900

    FEAT : 프로젝트 추가, 상세 페이지 MOCK API 연결 코드 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,29 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 import styled, { css } from "styled-components";
-import styles from '../styles/Home.module.css'
-import Layout from '../components/layout/layout'
-import Add from './project/add';
-import Main from './project/main'
+import styles from "../styles/Home.module.css";
+import Layout from "../components/layout/layout";
+import Add from "./project/add";
+import Main from "./project/main";
 
 const Home: NextPage = () => {
-  return (
-    <Layout>
-            <ExContainer>
-              <Main />
-            </ExContainer>
-    </Layout>
-  )
-}
+	return (
+		<Layout>
+			<ExContainer>
+				<Main />
+			</ExContainer>
+		</Layout>
+	);
+};
 
-export default Home
+export default Home;
 
 const ExContainer = styled.div`
-  position: absolute;
-  top: 0; left: 0;
-  width: 100%; height: auto;
-  padding : 7.7vw 0 0 16vw;
-  background: #F9F9F9;
-  z-index: -1;
-`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: auto;
+	padding: 7.7vw 0 0 16vw;
+	background: #f9f9f9;
+	z-index: -1;
+`;
