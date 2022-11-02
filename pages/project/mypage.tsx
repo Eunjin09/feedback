97d39efe7f8baa79805acf462e7bb45d3d312df8
yebin76 <103884098+yebin76@users.noreply.github.com>
2022-11-03 07:59:51 +0900

    마이페이지 화면 임시

---
+++ pages/project/mypage.tsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+export default function mypage() {
+  return (
+    <Wrap>
+      <MyinfoWrap>
+        <Title>
+          <h1>내 정보</h1>
+        </Title>
+
+        <InfoImg></InfoImg>
+        <Content>
+          <h1>홍길동</h1>
+          <div>이메일</div>
+          <div>전화번호</div>
+          <PasswardBtn>비밀번호 변경</PasswardBtn>
+          <SecessionBtn>회원탈퇴</SecessionBtn>
+        </Content>
+      </MyinfoWrap>
+    </Wrap>
+  );
+}
+
+const Wrap = styled.div``;
+const MyinfoWrap = styled.div``;
+const Title = styled.div`
+  h1 {
+    font-size: 48px;
+    line-height: 1;
+    margin-bottom: 34px;
+  }
+`;
+const InfoImg = styled.div`
+  width: 265px;
+  height: 265px;
+  background: #d9d9d9;
+`;
+const Content = styled.div`
+  h1 {
+    font-size: 32px;
+    margin-bottom: 32px;
+  }
+  div {
+    font-size: 24px;
+    line-height: 28px;
+  }
+`;
+const PasswardBtn = styled.button`
+  width: 225px;
+  height: 52px;
+  border-radius: 9px;
+  background: #444444;
+  font-size: 24px;
+  color: #ffffff;
+`;
+
+const SecessionBtn = styled.button`
+  font-size: 16px;
+  line-height: 18px;
+  color: #b63d3d;
+  margin-left: 15px;
+`;
